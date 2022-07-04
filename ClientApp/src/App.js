885ebfa0c302c9bfad5a1e217973e434389f8b23
Minh Cuong Vu <noedigsti@gmail.com>
2022-07-04 21:56:22 +1000

    included react-helmet & some meta tags

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import AppRoutes from './AppRoutes';
 import Layout from './components/Layout';
 import './custom.scss';
 
 export default function App() {
   return (
-    <Layout>
-      <Routes>
-        {AppRoutes.map((route, index) => {
-          const { element, ...rest } = route;
-          return <Route key={route} {...rest} element={element} />;
-        })}
-      </Routes>
-    </Layout>
+    <>
+      <Helmet>
+        <meta charset="utf-8" />
+        <meta name="author" content="Cuong Minh Vu" />
+        <meta
+          name="description"
+          content="My Community Directory is the one place people go to find community services and events so they can understand what's happening in their local community."
+        />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <title>
+          My Community Landmark — View community notes around you and add your
+          own!
+        </title>
+      </Helmet>
+      <Layout>
+        <Routes>
+          {AppRoutes.map((route, index) => {
+            const { element, ...rest } = route;
+            return <Route key={route} {...rest} element={element} />;
+          })}
+        </Routes>
+      </Layout>
+    </>
   );
 }
